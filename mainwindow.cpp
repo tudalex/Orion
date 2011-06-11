aadc12ddd8fbac7af3910da05c76bf2d6afb1e6b
Tudorica Constantin-Alexandru <tudalex@tudalex-virtual-machine.(none)>
2011-06-12 01:18:05 +0300

    Some mods

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -20,26 +20,30 @@ MainWindow::MainWindow(QWidget *parent) :
 
 void MainWindow::providersChanged()
 {
+    qDebug("provider has changed");
     if (!m_manager.providers().isEmpty()) {
         m_provider = m_manager.providerByUrl(QUrl(QString::fromAscii("http://api.opendesktop.org/v1/")));
         if (!m_provider.isValid()) {
 
             return;
         }
-        Attica::ItemJob<Attica::Person>* job = m_provider.requestPerson(QString::fromAscii("fregl"));
-        connect(job, SIGNAL(finished(Attica::BaseJob*)), SLOT(onContentRecieved(Attica::BaseJob*)));
-        job->start();
     }
+    Attica::ItemJob<Attica::Person>* job = m_provider.requestPerson(QString::fromAscii("fregl"));
+    connect(job, SIGNAL(finished(Attica::BaseJob*)), SLOT(onContentRecieved(Attica::BaseJob*)));
+    job->start();
 }
 
 void MainWindow::onContentRecieved(Attica::BaseJob *job)
 {
+    qDebug("Job finished");
     Attica::ItemJob<Attica::Person> *personJob = static_cast< Attica::ItemJob<Attica::Person> * >( job );
     if (personJob->metadata().error() == Attica::Metadata::NoError)
     {
         Attica::Person p(personJob->result());
         m_textEdit->setPlainText(p.lastName());
     }
+    else
+        qDebug("job didn't go well");
 }
 
 
