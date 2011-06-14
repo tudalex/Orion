e6ebd3ae8051e7bf4637033eab73a7c8fef84ff1
Tudorica Constantin-Alexandru <tudalex@gmail.com>
2011-06-14 05:31:47 +0300

    Some stuff that works on linux, still need to rename some variables & arrange the code a little

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,17 +4,48 @@
 #include <attica/person.h>
 #include <attica/itemjob.h>
 #include <QTextEdit>
+#include <QHBoxLayout>
+#include <QCommandLinkButton>
+#include "categorybutton.h"
+#include <attica/category.h>
+#include <attica/content.h>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent)
 {
+    QHBoxLayout *hbox = new QHBoxLayout;
+    m_categories = new QVBoxLayout;
+    hbox->addLayout(m_categories);
     m_textEdit = new QTextEdit();
     m_textEdit->setPlainText(QString::fromAscii("test"));
-    this->setCentralWidget(m_textEdit);
+    hbox->addWidget(m_textEdit);
+    QWidget *center = new QWidget;
+    center->setLayout(hbox);
+    this->setCentralWidget(center);
+    QCommandLinkButton *test = new QCommandLinkButton(QString::fromLatin1("buton de test"));
+    //categories->addWidget(test);
 
     connect(&m_manager, SIGNAL(defaultProvidersLoaded()), SLOT(providersChanged()));
     // tell it to get the default Providers
-    m_manager.loadDefaultProviders();
+    m_manager.addProviderFromXml(QString::fromLatin1("<provider>"
+                                 "<id>opendesktop</id>"
+                                 "<location>https://api.opendesktop.org/v1/</location>"
+                                 "<name>openDesktop.org</name>"
+                                 "<icon></icon>"
+                                 "<termsofuse>https://opendesktop.org/terms/</termsofuse>"
+                                 "<register>https://opendesktop.org/usermanager/new.php</register>"
+                                 "<services>"
+                                 "  <person ocsversion=\"1.3\" />"
+                                 "  <friend ocsversion=\"1.3\" />"
+                                 "  <message ocsversion=\"1.3\" />"
+                                 "  <activity ocsversion=\"1.3\" />"
+                                 "  <content ocsversion=\"1.3\" />"
+                                 "  <fan ocsversion=\"1.3\" />"
+                                 "  <knowledgebase ocsversion=\"1.3\" />"
+                                 "  <event ocsversion=\"1.3\" />"
+                                 "</services>"
+                                 "</provider>"));
+
 }
 
 
@@ -28,7 +59,7 @@ void MainWindow::providersChanged()
             qDebug("provider is not valid");
             return;
         }
-        Attica::ItemJob<Attica::Person>* job = m_provider.requestPerson(QString::fromAscii("fregl"));
+        Attica::ListJob<Attica::Category>* job = m_provider.requestCategories();
         connect(job, SIGNAL(finished(Attica::BaseJob*)), SLOT(onContentRecieved(Attica::BaseJob*)));
         job->start();
     }
@@ -38,14 +69,54 @@ void MainWindow::providersChanged()
 void MainWindow::onContentRecieved(Attica::BaseJob *job)
 {
     qDebug("Job finished");
-    Attica::ItemJob<Attica::Person> *personJob = static_cast< Attica::ItemJob<Attica::Person> * >( job );
+    Attica::ListJob<Attica::Category> *personJob = static_cast< Attica::ListJob<Attica::Category> * >( job );
     if (personJob->metadata().error() == Attica::Metadata::NoError)
     {
-        Attica::Person p(personJob->result());
-        m_textEdit->setPlainText(p.lastName());
+        int i = 0;
+        Attica::Category::List l(personJob->itemList());
+        for (QList <Attica::Category>::iterator it = l.begin(); it!=l.end(); ++ it)
+        {
+            Attica::Category *cat = new Attica::Category(*it);
+            CategoryButton *category_button = new CategoryButton(this,cat);
+            category_button->category=cat;
+
+            connect(category_button,SIGNAL(clicked()),this,SLOT(category_selected()));
+            m_categories->addWidget(category_button);
+            if (i>10)
+                break;
+            ++i;
+        }
+
     }
     else
         qDebug("job didn't go well");
+}
+
+void MainWindow::category_selected()
+{
+    CategoryButton *test =static_cast < CategoryButton *>(QObject::sender());
+    m_textEdit->setPlainText(test->category->name());
+    QList <Attica::Category> list;
+    list.append(*(test->category));
+    Attica::ListJob<Attica::Content>* job = m_provider.searchContents(list);
+    connect(job, SIGNAL(finished(Attica::BaseJob*)), SLOT(onContentListRecieved(Attica::BaseJob*)));
+    job->start();
+}
+
+void MainWindow::onContentListRecieved(Attica::BaseJob *job)
+{
+    Attica::ListJob<Attica::Content> *ContentListJob = static_cast< Attica::ListJob<Attica::Content> * >( job );
+    if (ContentListJob->metadata().error() == Attica::Metadata::NoError)
+    {
+        QString clist;
+        Attica::Content::List l(ContentListJob->itemList());
+        for (Attica::Content::List::iterator it = l.begin(); it!=l.end(); ++ it)
+        {
+            clist.append((static_cast < Attica::Content> (*it)).name());
+            clist.append(QChar::fromAscii('\n'));
+        }
+        m_textEdit->setPlainText(clist);
+    }
 }
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 #include <QTextEdit>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 #include <attica/provider.h>
 #include <attica/providermanager.h>
 
@@ -17,10 +19,14 @@ signals:
 public slots:
     void providersChanged();
     void onContentRecieved(Attica::BaseJob*);
+    void category_selected();
+    void onContentListRecieved(Attica::BaseJob*);
 private:
     Attica::Provider m_provider;
     Attica::ProviderManager m_manager;
     QTextEdit * m_textEdit;
+    QVBoxLayout *m_categories;
+
 
 };
 
